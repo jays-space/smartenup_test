7b3b90f8e66dc670c587325e3a1236149a9d266e
Jay Mondlana <contact.jays.space@gmail.com>
2024-08-25 22:44:05 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,25 @@ function App() {
     <div className="relative overflow-x-hidden">
       /**
       * #region KnowledgeHub
-      * ## this is a note for knowledge sharing
+      * # this is a note for knowledge sharing
+      * 
+      * --
+      * ## Heading 2
+      * ### Heading 3
+      * #### Heading 4
+      * ##### Heading 5
+      * ###### Heading 6
+      * --
+      *
+      * * point
+      * - point
+      * 1. point
+      *
+      * ---
+      *
+      * |column| column |
+      * --
+      * |1|2|
       * #endregion KnowledgeHub
       */
       <Navbar />
